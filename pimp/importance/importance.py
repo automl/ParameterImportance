automl/ParameterImportance

commit e07cc3c954a9a6c6f069d1583db48aa55df4716b

FIX #9 if runobjective = quality cutoff not necessary

pimp/importance/importance.py
@@ -5,6 +5,9 @@ import os
 import sys
 
 import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+
 import numpy as np
 
 from pimp.configspace import CategoricalHyperparameter, Configuration, FloatHyperparameter, IntegerHyperparameter
@@ -17,8 +20,6 @@ from pimp.evaluator.influence_models import InfluenceModel
 from pimp.utils import RunHistory, RunHistory2EPM4Cost, RunHistory2EPM4LogCost, Scenario, average_cost
 from smac.tae.execute_ta_run import StatusType
 
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
 
 
 
@@ -196,12 +197,12 @@ class Importance(object):
 
         params = self.scenario.cs.get_hyperparameters()
         num_params = len(params)
-        self.cutoff = self.scenario.cutoff
-        self.threshold = self.scenario.cutoff * self.scenario.par_factor
-        self.model = 'urfi'
 
         if self.scenario.run_obj == "runtime":
 
+            self.model = 'urfi'
+            self.cutoff = self.scenario.cutoff
+            self.threshold = self.scenario.cutoff * self.scenario.par_factor
             self.logged_y = True
             # if we log the performance data,
             # the RFRImputator will already get
@@ -227,6 +228,7 @@ class Importance(object):
                                                 StatusType.TIMEOUT, ],
                                             imputor=imputor)
         else:
+            self.model = 'rfi'
             rh2EPM = RunHistory2EPM4Cost(scenario=self.scenario,
                                          num_params=num_params,
                                          success_states=None,

examples/branin/branin.py
@@ -1,0 +1,26 @@
+import sys
+import numpy as np
+
+def main():
+    seed = sys.argv[5]
+    x = float(sys.argv[7])
+    y = float(sys.argv[9])
+    tmp = branin((x, y))
+    print('Result for SMAC: SUCCESS, -1, -1, %f, %s' % (tmp, seed))
+
+import numpy as np
+
+def branin(x):
+    x1 = x[0]
+    x2 = x[1]
+    a = 1.
+    b = 5.1 / (4.*np.pi**2)
+    c = 5. / np.pi
+    r = 6.
+    s = 10.
+    t = 1. / (8.*np.pi)
+    ret = a*(x2-b*x1**2+c*x1-r)**2+s*(1-t)*np.cos(x1)+s
+    return ret
+
+if __name__ == '__main__':
+    main()

examples/branin/branin_pcs.pcs
@@ -1,0 +1,2 @@
+x1 [-5,10] [0]
+x2 [0,15]  [0]

examples/branin/smac3-out/branin_pcs.pcs
@@ -1,0 +1,2 @@
+x1 [-5,10] [0]
+x2 [0,15]  [0]

commit ff1a6a092ee0c5454723100a520f7bac1dd79a22

FIX small issue introduced in e07cc3c

pimp/importance/importance.py
@@ -200,9 +200,9 @@ class Importance(object):
 
         if self.scenario.run_obj == "runtime":
 
-            self.model = 'urfi'
             self.cutoff = self.scenario.cutoff
             self.threshold = self.scenario.cutoff * self.scenario.par_factor
+            self.model = 'urfi'
             self.logged_y = True
             # if we log the performance data,
             # the RFRImputator will already get