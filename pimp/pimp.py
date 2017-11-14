d9980936e0510535f876c01bf2f44752eaad8d92
biedenka <biedenka@informatik.uni-freiburg.de>
2017-11-14 10:53:34 +0100

    ADD pimp now installable as command line tool

--- pimp/pimp.py
+++ pimp/pimp.py
@@ -1,5 +1,8 @@
 import os
 import json
+import datetime
+import logging
+import time
 from typing import List
 from typing import Dict
 from typing import Union
@@ -12,8 +15,15 @@ from smac.optimizer.objective import average_cost
 from smac.tae.execute_ta_run_aclib import StatusType
 from ConfigSpace.configuration_space import Configuration
 
-
 from pimp.importance.importance import Importance
+from pimp.utils.io.cmd_reader import CMDs
+
+
+__author__ = "Andre Biedenkapp"
+__copyright__ = "Copyright 2016, ML4AAD"
+__license__ = "3-clause BSD"
+__maintainer__ = "Andre Biedenkapp"
+__email__ = "biedenka@cs.uni-freiburg.de"
 
 
 class PIMP:
@@ -112,3 +122,45 @@ class PIMP:
                 json.dump(result, out_file, sort_keys=True, indent=4, separators=(',', ': '))
 
             self.imp.plot_results(name=os.path.join(save_folder, self.mode), show=show)
+
+
+def cmd_line_call():
+    """
+    Main Parameter importance script.
+    """
+    cmd_reader = CMDs()
+    args, misc_ = cmd_reader.read_cmd()  # read cmd args
+    logging.basicConfig(level=args.verbose_level)
+    ts = time.time()
+    ts = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H:%M:%S')
+    save_folder = 'PIMP_%s_%s' % (args.modus, ts)
+
+    importance = Importance(scenario_file=args.scenario_file, runhistory_file=args.history,
+                            parameters_to_evaluate=args.num_params,
+                            traj_file=args.trajectory, seed=args.seed,
+                            save_folder=save_folder,
+                            impute_censored=args.impute)  # create importance object
+    save_folder += '_run1'
+    with open(os.path.join(save_folder, 'pimp_args.json'), 'w') as out_file:
+        json.dump(args.__dict__, out_file, sort_keys=True, indent=4, separators=(',', ': '))
+    result = importance.evaluate_scenario(args.modus, sort_by=args.order)
+
+    if args.modus == 'all':
+        with open(os.path.join(save_folder, 'pimp_values_%s.json' % args.modus), 'w') as out_file:
+            json.dump(result[0], out_file, sort_keys=True, indent=4, separators=(',', ': '))
+        importance.plot_results(list(map(lambda x: os.path.join(save_folder, x.name.lower()), result[1])),
+                                result[1], show=False)
+        if args.table:
+            importance.table_for_comparison(evaluators=result[1], name=os.path.join(
+                save_folder, 'pimp_table_%s.tex' % args.modus), style='latex')
+        else:
+            importance.table_for_comparison(evaluators=result[1], style='cmd')
+    else:
+        with open(os.path.join(save_folder, 'pimp_values_%s.json' % args.modus), 'w') as out_file:
+            json.dump(result, out_file, sort_keys=True, indent=4, separators=(',', ': '))
+
+        importance.plot_results(name=os.path.join(save_folder, args.modus), show=False)
+
+
+if __name__ == '__main__':
+    cmd_line_call()
