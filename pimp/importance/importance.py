cf27ac48ca4d0d43b58d4e27141aad6bd03fca89
biedenka <biedenka@informatik.uni-freiburg.de>
2018-03-28 10:50:29 +0200

    MAINT information about logged data

--- pimp/importance/importance.py
+++ pimp/importance/importance.py
@@ -12,10 +12,10 @@ from tqdm import tqdm
 from smac.utils.util_funcs import get_types
 from smac.tae.execute_ta_run import StatusType
 from smac.epm.rfr_imputator import RFRImputator
-from smac.epm.rf_with_instances import RandomForestWithInstances
 
 from pimp.configspace import CategoricalHyperparameter, Configuration, \
     FloatHyperparameter, IntegerHyperparameter, impute_inactive_values
+from pimp.epm.base_epm import RandomForestWithInstances
 from pimp.epm.unlogged_epar_x_rfwi import UnloggedEPARXrfi
 from pimp.epm.unlogged_rfwi import Unloggedrfwi
 from pimp.evaluator.ablation import Ablation
@@ -284,19 +284,21 @@ class Importance(object):
             self.types, self.bounds = get_types(self.scenario.cs, self.scenario.feature_array)
             self._model = RandomForestWithInstances(self.types, self.bounds,
                                                     instance_features=self.scenario.feature_array,
-                                                    seed=12345)
+                                                    seed=12345, logged_y=self.logged_y)
         elif model_short_name == 'urfi':
             if not self._preprocessed:
                 self.types, self.bounds = get_types(self.scenario.cs, self.scenario.feature_array)
                 self._model = UnloggedEPARXrfi(self.types, self.bounds,
                                                instance_features=self.scenario.feature_array,
                                                seed=12345,
-                                               cutoff=self.cutoff, threshold=self.threshold)
+                                               cutoff=self.cutoff, threshold=self.threshold,
+                                               logged_y=self.logged_y)
             else:
                 self.types, self.bounds = get_types(self.scenario.cs, None)
                 self._model = Unloggedrfwi(self.types, self.bounds,
                                            instance_features=None,
-                                           seed=12345)
+                                           seed=12345,
+                                               logged_y=self.logged_y)
         self._model.rf_opts.compute_oob_error = True
 
     @property
